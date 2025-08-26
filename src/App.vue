70891f206960f5213cf35b6d68189934917a0555
luchengju <luchengju@hztianque.com>
2025-08-26 10:47:09 +0800

    构建界面:引入vant组件库、移动端适配、css变量主题定制

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button as vanButton } from 'vant'
+</script>
 
 <template>
-  <div>app</div>
+  <div class="abc">class="div1"</div>
+  <div class="div1">class="div2"</div>
+  <!-- 验证vant颜色被覆盖 -->
+  <van-button type="primary">按钮</van-button>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.abc {
+  color: var(--cp-primary);
+  background-color: var(--footer-color);
+}
+.div1 {
+  color: var(--hahaha);
+  background-color: var(--footer-color);
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
+import 'vant/lib/index.css'
 import './styles/main.scss'
 
 const app = createApp(App)
